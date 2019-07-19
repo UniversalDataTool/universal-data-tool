86d614f5695435536a6e1500e2bfe414e4a8136c
seveibar <seveibar@gmail.com>
2019-07-19 14:46:00 -0400

    additional test w/ data entry in OHA form

--- src/components/UniversalDataViewer/index.story.js
+++ src/components/UniversalDataViewer/index.story.js
@@ -7,42 +7,99 @@ import { action } from "@storybook/addon-actions"
 
 import UniversalDataViewer from "./"
 
-storiesOf("UniversalDataViewer", module).add("Basic", () => (
-  <UniversalDataViewer
-    onSaveTaskOutputItem={action("onSaveTaskOutputItem")}
-    hideHeader
-    oha={{
-      interface: {
-        type: "text_entity_recognition",
-        description: "Label words or phrases as food or hat.",
-        overlapAllowed: false,
-        labels: [
+storiesOf("UniversalDataViewer", module)
+  .add("NLP", () => (
+    <UniversalDataViewer
+      onSaveTaskOutputItem={action("onSaveTaskOutputItem")}
+      hideHeader
+      oha={{
+        interface: {
+          type: "text_entity_recognition",
+          description: "Label words or phrases as food or hat.",
+          overlapAllowed: false,
+          labels: [
+            {
+              id: "food",
+              displayName: "Food",
+              description: "Edible item."
+            },
+            {
+              id: "hat",
+              displayName: "Hat",
+              description: "Something worn on the head."
+            }
+          ]
+        },
+        taskData: [
           {
-            id: "food",
-            displayName: "Food",
-            description: "Edible item."
-          },
+            document:
+              "This strainer makes a great hat, I'll wear it while I serve spaghetti!"
+          }
+        ],
+        taskOutput: [
           {
-            id: "hat",
-            displayName: "Hat",
-            description: "Something worn on the head."
+            entities: [
+              { label: "hat", text: "strainer", start: 5, end: 13 },
+              { label: "food", text: "spaghetti", start: 59, end: 68 }
+            ]
           }
         ]
-      },
-      taskData: [
-        {
-          document:
-            "This strainer makes a great hat, I'll wear it while I serve spaghetti!"
-        }
-      ],
-      taskOutput: [
-        {
-          entities: [
-            { label: "hat", text: "strainer", start: 5, end: 13 },
-            { label: "food", text: "spaghetti", start: 59, end: 68 }
-          ]
-        }
-      ]
-    }}
-  />
-))
+      }}
+    />
+  ))
+  .add("Data Entry", () => (
+    <UniversalDataViewer
+      onSaveTaskOutputItem={action("onSaveTaskOutputItem")}
+      hideHeader
+      oha={{
+        interface: {
+          type: "data_entry",
+          description: "",
+          surveyjs: {
+            pages: [
+              {
+                name: "page1",
+                elements: [
+                  {
+                    type: "radiogroup",
+                    name: "group_letter",
+                    title: "Group Letter",
+                    choices: [
+                      {
+                        value: "A",
+                        text: "A"
+                      },
+                      {
+                        value: "B",
+                        text: "B"
+                      },
+                      {
+                        value: "C",
+                        text: "C"
+                      }
+                    ]
+                  },
+                  {
+                    type: "text",
+                    name: "feedback",
+                    title: "Feedback"
+                  }
+                ]
+              }
+            ]
+          }
+        },
+        taskData: [
+          {
+            description: "Cucumber"
+          }
+        ],
+        taskOutput: [
+          {
+            group_letter: "A",
+            feedback: "this is some feedback"
+          }
+        ]
+      }}
+    />
+  ))
