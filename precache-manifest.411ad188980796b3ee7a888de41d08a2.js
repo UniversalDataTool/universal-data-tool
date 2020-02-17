620379a94e5178df111b071edd6d2e2e586fc4ef
seveibar <seveibar@gmail.com>
2020-02-17 18:41:56 -0500

    Updates

---
+++ precache-manifest.411ad188980796b3ee7a888de41d08a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79f5c836c0edeb30c80c7470eaa1911",
+    "url": "/index.html"
+  },
+  {
+    "revision": "501dac6c6b1f8e69bedc",
+    "url": "/static/css/main.bf259f8e.chunk.css"
+  },
+  {
+    "revision": "f35fb64d41e934c923ef",
+    "url": "/static/js/2.032f2295.chunk.js"
+  },
+  {
+    "revision": "501dac6c6b1f8e69bedc",
+    "url": "/static/js/main.73d284e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.23ade1149f556fde8f9a1ec339cd5875.js"
+  "/precache-manifest.411ad188980796b3ee7a888de41d08a2.js"
 );
 
 self.addEventListener('message', (event) => {
