406226d791892b4bf2d7f45a3a5d681ad0aebbc6
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2020-02-28 22:41:21 +0300

    Fixed form questions

--- src/components/ConfigureNLP/index.js
+++ src/components/ConfigureNLP/index.js
@@ -4,44 +4,19 @@ import Survey from "material-survey/components/Survey"
 const form = {
   questions: [
     {
-        name: "transcriptionType",
-        title: "Region Types Allowed",
-        description: "What types of regions can be drawn on the image.",
-        type: "dropdown",
-        choices: ["simple", "proper"]
+        name: "description",
+        title: "Description",
+        type: "multiline-text",
     },
     {
-        name: "phraseBank",
-        title: "Region Types Allowed",
-        description: "What types of regions can be drawn on the image.",
-        hasOther: true,
-        type: "checkbox",
-        choices: [
-            "bounding-box",
-            "polygon",
-            "point"
-        ],
-    },
-    {
-        name: "onlyUseWordsInPhraseBank",
-        title: "Region Types Allowed",
-        description: "What types of regions can be drawn on the image.",
-        type: "dropdown",
-        choices: ["bounding-box", "polygon", "point"]
-    },
+        name: "overlapAllowed",
+        title: "Overlap Allowed",
+        type: "boolean"
+    }
   ]
 }
 
-export default ({ iface, onChange }) => {
-    // const defaultAnswers = useMemo(
-    //     () =>({
-    //         transcriptionType: '',
-    //         phraseBank: '',
-    //         onlyUseWordsInPhraseBank: ''
-    //     }),
-    //     []
-    // )
-    
+export default ({ iface, onChange }) => {    
     return(
         <Survey
             noActions
