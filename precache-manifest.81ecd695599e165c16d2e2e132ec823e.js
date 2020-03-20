b2dcb6a41f5a4eac833c62801a1b7dee9c080759
seveibar <seveibar@gmail.com>
2020-03-20 16:49:07 -0400

    Updates

---
+++ precache-manifest.81ecd695599e165c16d2e2e132ec823e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbf2414b9f039d8a2596bd5751b31a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ee0da9d4e7117c34148",
+    "url": "/static/css/main.bf259f8e.chunk.css"
+  },
+  {
+    "revision": "2e37c35f23a68cd02c3d",
+    "url": "/static/js/2.8ab65344.chunk.js"
+  },
+  {
+    "revision": "9ee0da9d4e7117c34148",
+    "url": "/static/js/main.e1eb1fb0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70957b8fbabfa047ce72fea448615e93.js"
+  "/precache-manifest.81ecd695599e165c16d2e2e132ec823e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
