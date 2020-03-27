d184181c9e74c08ce56fb8e2173e0930161b65e9
Marc <marcpaquet2@hotmail.com>
2020-03-27 15:12:18 -0400

    table

--- src/components/ImportFromS3Dialog/index.js
+++ src/components/ImportFromS3Dialog/index.js
@@ -1,23 +1,39 @@
 // @flow weak
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useCallback, useMemo } from "react"
 import { styled } from "@material-ui/core/styles"
 import SimpleDialog from "../SimpleDialog"
 import DataTable from "react-data-table-component"
+import Radio from '@material-ui/core/Radio';
 
 import Amplify, { Storage } from 'aws-amplify'
 import isEmpty from "../../utils/isEmpty"
 
-
+/*
+Storage.get(`${folder}/data/${obj.key.split('/data/')[1]}`, { expires: 24 * 60 * 60, level: 'private' })
+                .then(result => {
+                  addImageUrl(result, folder, obj.key.split('/data/')[1])
+                  return result
+                })
+                .catch(err => {
+                  console.log('error getting link for s3 image', err)
+                  return null
+                })
+*/
 
 const TextArea = styled("textarea")({
   width: "100%",
   minHeight: 300
 })
 
+const expandedDataColumns = [{ name: 'Data', selector: 'data', sortable: true }]
+
+const expandedAnnotationsColumns = [{ name: 'Annotations', selector: 'annotation' }]
+
+const columns = [{ name: 'Projects', selector: 'folder', sortable: true }]
+
+
 const ExpandedRow = ({ data }) => {
   const { rowData, rowAnnotations, ...notImportant } = data
-  const expandedDataColumns = [{ name: 'Data', selector: 'data', sortable: true }]
-  const expandedAnnotationsColumns = [{ name: 'Annotations', selector: 'annotation' }]
 
   return (
     <>
@@ -52,6 +68,28 @@ export default ({ open, onClose, onAddSamples, authConfig, user }) => {
   const [content, changeContent] = useState("")
   const [s3Content, changeS3Content] = useState(null)
   const [dataForTable, changeDataForTable] = useState(null)
+  const [showImage, changeShowImage] = useState(false)
+  const [resolvedUrls, changeResolvedUrls] = useState({})
+  const [toggleCleared, setToggleCleared] = useState(false);
+  const [displayTable, setDisplayTable] = useState(false)
+
+  let _dataForTable = {}
+
+  const addImageUrl = (result, folder, imgName) => {
+    changeResolvedUrls((prevState) => ({
+      ...prevState, [`${folder}-${imgName}`]: result
+    }))
+  }
+
+  const handleRowSelected = () => {
+    setToggleCleared((prevState) => !prevState)
+    console.log('triggered')
+    //setToggleCleared((prevState) => !prevState)
+  }
+
+  useEffect(() => {
+    console.log(toggleCleared)
+  }, [toggleCleared])
 
   useEffect(() => {
     if (isEmpty(user)) {
@@ -62,42 +100,38 @@ export default ({ open, onClose, onAddSamples, authConfig, user }) => {
       Storage.list('', { level: 'private' })
         .then(result => {
           changeS3Content(result)
-          changeDataForTable(
-            result.filter((obj) => {
-              return (obj.key.endsWith('/') & obj.key.split('/').length === 2)
+          _dataForTable = result.filter((obj) => {
+            return (obj.key.endsWith('/') & obj.key.split('/').length === 2)
+          }).map((obj, index) => {
+            const folder = obj.key.split('/')[0]
+            const rowDataContent = result.filter((obj) => {
+              return (obj.key.startsWith(`${folder}/data/`) & !obj.key.endsWith('/'))
             }).map((obj) => {
-              const folder = obj.key.split('/')[0]
-              const rowDataContent = result.filter((obj) => {
-                return (obj.key.startsWith(`${folder}/data/`) & !obj.key.endsWith('/'))
-              }).map((obj) => {
-                return (
-                  { data: obj.key.split('/data/')[1] }
-                )
-              })
-              const rowAnnotationsContent = result.filter((obj) => {
-                return (obj.key.startsWith(`${folder}/annotations/`) & !obj.key.endsWith('/'))
-              }).map((obj) => {
-                return (
-                  { annotation: obj.key.split('/annotations/')[1] }
-                )
-              })
-              return ({
-                folder: folder,
-                rowData: rowDataContent,
-                rowAnnotations: rowAnnotationsContent
-              })
+              return (
+                {
+                  data: obj.key.split('/data/')[1],
+                }
+              )
             })
-          )
+            const rowAnnotationsContent = result.filter((obj) => {
+              return (obj.key.startsWith(`${folder}/annotations/`) & !obj.key.endsWith('/'))
+            }).map((obj) => {
+              return (
+                { annotation: obj.key.split('/annotations/')[1] }
+              )
+            })
+            return ({
+              id: `${index}`,
+              folder: folder,
+              rowData: rowDataContent,
+              rowAnnotations: rowAnnotationsContent
+            })
+          })
+          changeDataForTable(_dataForTable)
         })
         .catch(err => console.log(err))
     }
   }, [user])
-
-  useEffect(() => {
-
-  }, [dataForTable])
-
-  const columns = [{ name: 'Projects', selector: 'folder', sortable: true }]
 
   return (
     <SimpleDialog
@@ -108,40 +142,19 @@ export default ({ open, onClose, onAddSamples, authConfig, user }) => {
         {
           text: "Add Samples",
           onClick: () => {
-            onAddSamples(
-              content
-                .split("\n")
-                .map(l => l.trim())
-                .filter(Boolean)
-                .map(s => {
-                  const extension = s.split(".").slice(-1)[0]
-                  switch (extension.toLowerCase()) {
-                    case "png":
-                    case "jpg":
-                    case "gif":
-                    case "jpeg":
-                    case "bmp": {
-                      return { imageUrl: s }
-                    }
-                    case "pdf": {
-                      return { pdfUrl: s }
-                    }
-                    default: {
-                      // TODO throw error or toast
-                      console.error("extension not recognized")
-                      return null
-                    }
-                  }
-                })
-            )
+            console.log('adding samples')
           }
         }
       ]}
     >
       {!isEmpty(dataForTable) && (
-        <DataTable
-          expandableRowsComponent={<ExpandedRow />}
+        < DataTable
           expandableRows
+          expandableRowsComponent={<ExpandedRow />}
+          selectableRows
+          selectableRowsHighlight
+          selectableRowsNoSelectAll
+          selectableRowsComponent={Radio}
           dense
           noHeader
           columns={columns}
@@ -151,6 +164,7 @@ export default ({ open, onClose, onAddSamples, authConfig, user }) => {
           paginationRowsPerPageOptions={[10, 20, 25, 50, 100, 200]}
         />
       )}
+
     </SimpleDialog>
   )
 }
