f8f6e7c24b53730b5a746c049c1de08f063ba0ae
seveibar <seveibar@gmail.com>
2020-04-03 00:16:17 -0400

    fixes to release process, commit back to master

--- .releaserc.js
+++ .releaserc.js
@@ -5,5 +5,13 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     ["@semantic-release/npm", { npmPublish: false }],
     "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
   ],
 }
