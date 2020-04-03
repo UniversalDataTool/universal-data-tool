d22c5033c3dad12ccfb88661132e08fd491f9799
seveibar <seveibar@gmail.com>
2020-04-03 06:05:31 -0400

    Updates

---
+++ precache-manifest.eaaccbc7683342f346d03fc8010f287f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b773ce686cdb20853bb75adea90b63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b0e02c7252910b484c6",
+    "url": "/static/css/main.bf259f8e.chunk.css"
+  },
+  {
+    "revision": "62bce5365187a1a47766",
+    "url": "/static/js/2.04f2b674.chunk.js"
+  },
+  {
+    "revision": "cbfa55c6284cdf578e23950b996cea70",
+    "url": "/static/js/2.04f2b674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0e02c7252910b484c6",
+    "url": "/static/js/main.049f9316.chunk.js"
+  },
+  {
+    "revision": "b7bde68028857a54eb9b",
+    "url": "/static/js/runtime-main.242e6c32.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.116fcbaebb06566c767c19686c06d9f0.js"
+  "/precache-manifest.eaaccbc7683342f346d03fc8010f287f.js"
 );
 
 self.addEventListener('message', (event) => {
