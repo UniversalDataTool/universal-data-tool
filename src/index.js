2f69adcde21f3a6561032e013a27d2d515a58c45
seveibar <seveibar@gmail.com>
2020-04-12 00:14:26 -0400

    prevent page navigation

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 
+window.addEventListener("beforeunload", function (e) {
+  var confirmationMessage =
+    "It looks like you have been editing something. " +
+    "If you leave before saving, your changes will be lost."
+
+  ;(e || window.event).returnValue = confirmationMessage //Gecko + IE
+  return confirmationMessage //Gecko + Webkit, Safari, Chrome etc.
+})
+
 ReactDOM.render(<App />, document.getElementById("root"))
