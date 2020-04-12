8aa81b3968641fbc3a9c7746430451023ed8358f
Severin Ibarluzea <seve700@gmail.com>
2020-04-12 01:35:44 -0400

    fix: Prevent Loss of Work from Accidental Page Navigation (#77)
    * prevent page navigation
    
    * refactor to hook, don't do it on welcome page

---
+++ src/utils/use-prevent-navigation.js
@@ -0,0 +1,22 @@
+// @flow weak
+
+import { useEffect } from "react"
+
+function preventNavigation(e) {
+  var confirmationMessage =
+    "It looks like you have been editing something. " +
+    "If you leave before saving, your changes will be lost."
+
+  ;(e || window.event).returnValue = confirmationMessage //Gecko + IE
+  return confirmationMessage //Gecko + Webkit, Safari, Chrome etc.
+}
+
+export default (shouldPreventNavigation = true) => {
+  useEffect(() => {
+    if (!shouldPreventNavigation) return
+    window.addEventListener("beforeunload", preventNavigation)
+    return () => {
+      window.removeEventListener("beforeunload", preventNavigation)
+    }
+  }, [shouldPreventNavigation])
+}
