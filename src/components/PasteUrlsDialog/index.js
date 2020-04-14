1eeeaf5a3e0d345f28926ec8c968e94e0f6bf91d
seveibar <seveibar@gmail.com>
2020-04-14 05:26:58 -0400

    auto detect / image url select on paste urls. fix for download csv w/ embedded arrays

--- src/components/PasteUrlsDialog/index.js
+++ src/components/PasteUrlsDialog/index.js
@@ -2,14 +2,31 @@
 import React, { useState } from "react"
 import { styled } from "@material-ui/core/styles"
 import SimpleDialog from "../SimpleDialog"
+import Select from "react-select"
 
+const SelectContainer = styled("div")({
+  marginTop: 8,
+  marginBottom: 16,
+})
 const TextArea = styled("textarea")({
   width: "100%",
   minHeight: 300,
 })
 
+const autoDetectOptions = [
+  {
+    label: "Auto Detect File Type",
+    value: "auto_detect",
+  },
+  {
+    label: "Image URLs",
+    value: "images",
+  },
+]
+
 export default ({ open, onClose, onAddSamples }) => {
   const [content, changeContent] = useState("")
+  const [urlType, changeURLType] = useState("auto_detect")
   return (
     <SimpleDialog
       title="Paste Urls"
@@ -25,10 +42,10 @@ export default ({ open, onClose, onAddSamples }) => {
                 .map((l) => l.trim())
                 .filter(Boolean)
                 .map((s) => {
-                  let extension = s
-                    .replace(/\?.*/g, "")
-                    .split(".")
-                    .slice(-1)[0]
+                  if (urlType === "images") {
+                    return { imageUrl: s }
+                  }
+                  let extension = s.replace(/\?.*/g, "").split(".").slice(-1)[0]
                   if (s.includes("gstatic.com/images")) {
                     extension = "jpg"
                   }
@@ -60,6 +77,15 @@ export default ({ open, onClose, onAddSamples }) => {
         },
       ]}
     >
+      <SelectContainer>
+        <Select
+          options={autoDetectOptions}
+          defaultValue={autoDetectOptions[0]}
+          onChange={(opt) => {
+            changeURLType(opt.value)
+          }}
+        />
+      </SelectContainer>
       <TextArea
         onChange={(e, v) => changeContent(e.target.value)}
         placeholder={"Paste URLs here\nOne URL per line"}
