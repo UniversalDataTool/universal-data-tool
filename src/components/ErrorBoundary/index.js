aa69030f96fc6f93b323e19cd1c1a615dd39da10
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2020-04-14 16:18:45 +0300

    Added regex for accepting desktop application urls

--- src/components/ErrorBoundary/index.js
+++ src/components/ErrorBoundary/index.js
@@ -10,7 +10,7 @@ import * as Sentry from "@sentry/browser"
 
 Sentry.init({
   dsn: "https://bc19fbac222243f08f0abaf6d66f2034@sentry.io/5182632",
-  whitelistUrls: ["https://universaldatatool.com"],
+  whitelistUrls: ["https://universaldatatool.com", "^file:?:"],
 })
 
 const Container = styled("div")({
