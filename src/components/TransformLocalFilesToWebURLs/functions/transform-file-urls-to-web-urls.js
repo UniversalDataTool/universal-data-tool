ca5bfa334b283b44add64dbf060d6d1882175ded
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2020-04-20 22:05:20 +0000

    Changed all strategy

--- src/components/TransformLocalFilesToWebURLs/functions/transform-file-urls-to-web-urls.js
+++ src/components/TransformLocalFilesToWebURLs/functions/transform-file-urls-to-web-urls.js
@@ -1,27 +1,74 @@
-import uploadFileToTransferSh from './upload-file-to-trasfersh'
+import uploadFileToTransferSh from './upload-file-to-transfersh'
 import splitFileNameFromFileURL from './split-file-name-from-file-url'
-import putFileURLsToAnArray from './put-file-urls-to-an-array'
 import { setIn } from "seamless-immutable";
 
-const tranfomFileURLsToWebURLs = async({ oha, onChangeOHA, setProgress, remote }) => {
-    const newOhaFromUploadedFiles = []
+// TODO move to utils directory
+const idify = (s) =>
+  s
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "_")
+    .replace(/_+/g, "_")
 
-    const fileURLsArray = putFileURLsToAnArray(oha.taskData)
-
-    const filesCount = fileURLsArray.length
-    const progressUnit = 100 / filesCount
-    for (const fileURL of fileURLsArray) {
-        const indexOfFileURL = (fileURLsArray.indexOf(fileURL)) + 1
-        const halfProgressOfUnit = (progressUnit / 2) * indexOfFileURL
-        setProgress(halfProgressOfUnit)
-        const fileName = splitFileNameFromFileURL(fileURL)
-        const webURLOfUploadedFile = await uploadFileToTransferSh({ fileName, fileURL, remote })
-        const newOhaItem = { videoUrl: webURLOfUploadedFile } //TODO: change this with real ohaItem
-        newOhaFromUploadedFiles.push(newOhaItem)
-        const fullProgressOfUnit = progressUnit * indexOfFileURL
-        setProgress(fullProgressOfUnit)
-    }
-    onChangeOHA(setIn(oha, ["taskData"], newOhaFromUploadedFiles));
+const getFileURLKey = (item) => {
+    if (item.imageUrl) return "imageUrl"
+    if (item.videoUrl) return "videoUrl"
+    if (item.audioUrl) return "audioUrl"
+    return null
 }
 
-export default tranfomFileURLsToWebURLs
+const transformFileURLsToWebURLs = async({ oha, onChangeOHA, setProgress, remote }) => {
+    const newTaskData = []
+
+    //const fileURLsArray = putFileURLsToAnArray(oha.taskData)
+
+    // const filesCount = fileURLsArray.length
+    const progressUnit = 100 / oha.taskData.length
+    for (let taskDataIndex = 0; taskDataIndex < oha.taskData.length; taskDataIndex++) {
+        const taskDataItem = oha.taskData[taskDataIndex]
+        
+        const fileURLKey = getFileURLKey(taskDataItem)
+        
+        if (!fileURLKey) {
+            newTaskData.push(taskDataItem)
+            continue
+        }
+        
+        const fileURL = taskDataItem[fileURLKey]
+        
+        if (!fileURL.startsWith("file://")) {
+            newTaskData.push(taskDataItem)
+            continue
+        }
+        
+        
+        const halfProgressOfUnit = progressUnit * taskDataIndex
+        setProgress(halfProgressOfUnit)
+        
+        const fileName = idify(splitFileNameFromFileURL(fileURL))
+        
+        const webURLOfUploadedFile = await uploadFileToTransferSh({ fileName, fileURL, remote })
+            .catch(e => (console.warn(e), null))
+        
+        if (!webURLOfUploadedFile) {
+            newTaskData.push(taskDataItem)
+            continue
+        }
+        
+        newTaskData.push({
+            ...taskDataItem,
+            [fileURLKey]: webURLOfUploadedFile
+        })
+        
+        
+        // const indexOfFileURL = (fileURLsArray.indexOf(fileURL)) + 1
+        // const webURLOfUploadedFile = await uploadFileToTransferSh({ fileName, fileURL, remote })
+        // const newOhaItem = { videoUrl: webURLOfUploadedFile } //TODO: change this with real ohaItem
+        // newTaskData.push(newOhaItem)
+        // const fullProgressOfUnit = progressUnit * indexOfFileURL
+        // setProgress(fullProgressOfUnit)
+    }
+    setProgress(100)
+    onChangeOHA(setIn(oha, ["taskData"], newTaskData));
+}
+
+export default transformFileURLsToWebURLs
