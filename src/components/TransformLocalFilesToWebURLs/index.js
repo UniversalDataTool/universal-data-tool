6be0d1077870a603fa107deaf18de2bd406402af
Puskuruk <tamayeseruysal@gmail.com>
2020-04-27 18:32:08 +0300

    fix: Feature/transform local files to web urls (#96)
    * Created new folder for functions of converting local files to web urls and created transform-file-urls-to-web-urls function for blue-print of that feature
    
    * Adding function to splitting file names from local urls
    
    * Created put file urls to an array function to splitting oha taskdata and putting them to an array
    
    * Added function to upload files to our file storage service
    
    * Created an button for transform local files to web urls and added to transformpage
    
    * Created view with explaining how it works in comments
    
    * Fixed typo
    
    * fixed typo on filename
    
    * Added axios and form-data
    
    * Deleted unneceserry function
    
    * changed all strategy about uploading files
    
    * Changed all strategy
    
    * There was a typo... :/
    
    * Deleted commented unnecessery code
    
    * Splitted getFileUrlKey to the new file
    
    * Made pr prettier
    
    * Added package-lock.json
    
    * Deleted explanation from component index.js and fixed typo
    
    * Deleted unnecessery console.log

---
+++ src/components/TransformLocalFilesToWebURLs/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react"
+import SimpleDialog from "../SimpleDialog"
+import transformFileURLsToWebURLs from "./functions/transform-file-urls-to-web-urls"
+import ProgressBar from "../ProgressBar"
+import useElectron from "../../utils/use-electron"
+
+const TransformLocalFilesToWebURLs = ({ open, onClose, oha, onChangeOHA }) => {
+  const [progress, setProgress] = useState(0)
+  const { remote } = useElectron() || {}
+
+  return (
+    <SimpleDialog
+      open={open}
+      onClose={onClose}
+      title="Convert Local Files to Web URLs"
+      actions={[
+        {
+          text: "Convert Local Files to Web URLs",
+          onClick: async () =>
+            await transformFileURLsToWebURLs({
+              oha,
+              onChangeOHA,
+              setProgress,
+              remote,
+            }),
+        },
+      ]}
+    >
+      This transformation will upload your local samples to the cloud Note: This
+      can take a little while.
+      <ProgressBar progress={progress} />
+    </SimpleDialog>
+  )
+}
+
+export default TransformLocalFilesToWebURLs
