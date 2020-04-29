12bbb9538f6b9780a7896e1aa49ca5609a127e99
Severin Ibarluzea <seve700@gmail.com>
2020-04-29 09:48:45 -0400

    fix: Vanilla Script Support and Waveform Component (#109)
    * initial element
    
    * region changes propogate up
    
    * vanilla support added
    
    * prettier fix for waveform
    
    Co-authored-by: Tamay Eser Uysal <tamayeseruysal@gmail.com>

---
+++ src/vanilla/open.story.js
@@ -0,0 +1,43 @@
+// @flow
+
+import React, { useEffect } from "react"
+
+import { storiesOf } from "@storybook/react"
+import { action } from "@storybook/addon-actions"
+
+storiesOf("Vanilla Open", module).add("Basic", () => {
+  useEffect(() => {
+    const script = document.createElement("script")
+    script.onload = () => {
+      window.UniversalDataTool.open({
+        container: "udt",
+        udt: {
+          interface: {
+            type: "image_segmentation",
+          },
+          taskData: [
+            {
+              imageUrl:
+                "https://s3.amazonaws.com/asset.workaround.online/example-jobs/sticky-notes/image1.jpg",
+            },
+          ],
+        },
+        onSaveTaskOutputItem: (index, output) => {
+          action("onSaveTaskOutputItem")(output)
+        },
+      })
+    }
+    script.type = "text/javascript"
+    script.async = true
+    script.src = "http://localhost:1234/index.js"
+    window.document.body.appendChild(script)
+  }, [])
+
+  return (
+    <div>
+      you must be running the vanilla dev server for these examples to work
+      <script src="http://localhost:1234/index.js"></script>
+      <div id="udt"></div>
+    </div>
+  )
+})
