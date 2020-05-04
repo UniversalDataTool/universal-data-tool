91cd5fae117d5edc63a6f64f56b122eaae4a5daf
Cédric Jean <cedric_jean@outlook.com>
2020-05-04 10:34:13 -0400

    Revert "last changes"
    This reverts commit a2390612cc57c0c4829bfac72f16b1d11baead35.

--- src/components/AddAuthFromTemplateDialog/index.js
+++ src/components/AddAuthFromTemplateDialog/index.js
@@ -84,7 +84,7 @@ export default ({ open, onClose, onSelect, onFinish, onAuthConfigured }) => {
   const [dialogTitle, setDialogTitle] = useState("Add Authentification")
   const [errors, addError] = useErrors()
 
-  const validateAuthProvider = async (answers) => {
+  const validateAuthProvider = (answers) => {
     if (answers.provider === "AWS") {
       const config = {
         Auth: {
@@ -113,7 +113,6 @@ export default ({ open, onClose, onSelect, onFinish, onAuthConfigured }) => {
             onAuthConfigured(config)
             onClose()
           })
-          console.log(await Auth.verifyCurrentUserAttribute())
       } catch (err) {
         addError("Invalid Cognito config")
       }
