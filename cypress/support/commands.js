18a2496f0cbb732e995fbee08d83217d5ea52add
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2020-05-08 03:18:03 +0300

    Added an extension to cypress to finding visual diffs

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command'
+
+addMatchImageSnapshotCommand()
