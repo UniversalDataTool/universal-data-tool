cc539d737bcbe7792f40097257a80c649dcd36d9
seveibar <seveibar@gmail.com>
2020-05-07 22:18:42 -0400

    add fake npm build step to fix github commit issue

--- .releaserc.js
+++ .releaserc.js
@@ -3,15 +3,16 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/npm", { npmPublish: true, pkgRoot: "lib" }],
+    ["@semantic-release/npm", { npmPublish: false }],
     "@semantic-release/github",
     [
       "@semantic-release/git",
       {
-        assets: ["../package.json"],
+        assets: ["package.json"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
+    ["@semantic-release/npm", { npmPublish: true, pkgRoot: "lib" }],
   ],
 }
