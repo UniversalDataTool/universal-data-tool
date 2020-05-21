c1174e4cb3a9267a225ca78d9ba018be5b01ef61
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2020-05-21 21:09:07 +0300

    Added test for aws cognito

---
+++ cypress/integration/aws-cognito-authentication.spec.js
@@ -0,0 +1,52 @@
+const awsCredentials = [
+    "identity_pool_id",
+    "region",
+    "user_pool_id",
+    "user_pool_web_client_id",
+    "awss3_bucket",
+    "awss3_region"
+]
+
+describe("AWS Cognito authentication", () => {
+    it("should be able to open application", () => {
+        cy.visit("http://localhost:6001")
+    })
+
+    it("should be able to open 'Add Authentification for AWS - Cognito' modal", () => {
+        cy.contains("Add Authentification").click()
+    })
+
+    it("should be able to open 'AWS - Cognito' modal", () => {
+        cy.contains("AWS - Cognito").click()
+    })
+
+    it("should be able to fill AWS Cognito credentials", () => {
+        cy.get("input[type=text]").each(($el, index, $list) => {
+            if (index > 0) {
+                const credentialKey = `CYPRESS_AWS_COGNITO_${awsCredentials[index - 1]}`
+                const credential = Cypress.env(credentialKey)
+                cy.get($el).type(credential)
+            }
+        })
+    })
+
+    it("should be able to press 'Complete' button", () => {
+        cy.contains("Complete").click()
+    })
+
+    it("should be able to open 'Login with Cognito' drawer", () => {
+        cy.contains("Login with Cognito").click()
+    })
+
+    it("should be able login to 'Cognito'", () => {
+        const username = Cypress.env("CYPRESS_AWS_COGNITO_username")
+        cy.get("input[id=username]").type(username)
+        const password = Cypress.env("CYPRESS_AWS_COGNITO_password")
+        cy.get("input[id=password]").type(password)
+        cy.contains("Sign In").click()
+    })
+
+    it("should be able to see 'Logout'", () => {
+        cy.contains("Logout")
+    })
+})
