abf6f5e50284b9bd86491e1e1598588da0138722
seveibar <seveibar@gmail.com>
2020-05-23 17:08:25 -0400

    keyboard shortcut manager dialog component complete

--- src/components/KeyboardShortcutManagerDialog/index.js
+++ src/components/KeyboardShortcutManagerDialog/index.js
@@ -1,65 +1,101 @@
-import React from "react"
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState, useEffect, useMemo } from "react"
+import { makeStyles } from "@material-ui/core/styles"
 import SimpleDialog from "../SimpleDialog"
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from "@material-ui/core/Paper"
+import Table from "@material-ui/core/Table"
+import TableBody from "@material-ui/core/TableBody"
+import TableCell from "@material-ui/core/TableCell"
+import TableContainer from "@material-ui/core/TableContainer"
+import TableHead from "@material-ui/core/TableHead"
+import TableRow from "@material-ui/core/TableRow"
+import Button from "@material-ui/core/Button"
+import EditIcon from "@material-ui/icons/Edit"
 
 const useStyles = makeStyles({
-  table: { minWidth: 650 }
+  table: {},
 })
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+const hotkeyIconStyle = { marginLeft: 8, width: 16, height: 16 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-
-export default ({ open, onClose }) => {
+export default ({ open, onClose, onChangeHotkey, hotkeyList }) => {
   const classes = useStyles()
+  const [selectedHotKey, setSelectedHotKey] = useState()
+  const [{ modifier, key }, setCurrentKeyCombo] = useState({})
+
+  useEffect(() => {
+    if (!selectedHotKey) return
+    function keyListener(e) {
+      if (["shift", "ctrl", "cmd", "alt"].includes(e.key.toLowerCase())) {
+        setCurrentKeyCombo({ modifier: e.key.toLowerCase(), key })
+      }
+      if (e.key.length === 1) {
+        setCurrentKeyCombo({ modifier, key: e.key.toLowerCase() })
+      }
+    }
+    window.addEventListener("keydown", keyListener)
+    return () => {
+      window.removeEventListener("keydown", keyListener)
+    }
+  }, [selectedHotKey, key, modifier, setCurrentKeyCombo])
+
+  useEffect(() => {
+    if (!key) return
+    if (modifier) {
+      onChangeHotkey(selectedHotKey, `${modifier} + ${key}`)
+    } else {
+      onChangeHotkey(selectedHotKey, key)
+    }
+    setCurrentKeyCombo({})
+    setSelectedHotKey(null)
+  }, [modifier, key, onChangeHotkey, selectedHotKey])
+
   return (
     <SimpleDialog
       title="Manage Keyboard Shortcuts"
       open={open}
       onClose={onClose}
     >
-      <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+      <TableContainer>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Action</TableCell>
+              <TableCell align="right">Keyboard Shortcut</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {hotkeyList.map((hotkey) => (
+              <TableRow key={hotkey.description}>
+                <TableCell>{hotkey.description}</TableCell>
+                <TableCell align="right">
+                  <Button
+                    onClick={() => {
+                      if (selectedHotKey !== hotkey) {
+                        setSelectedHotKey(hotkey)
+                        setCurrentKeyCombo({})
+                        return
+                      }
+                    }}
+                    style={{ minWidth: 0 }}
+                  >
+                    <b>
+                      {selectedHotKey === hotkey
+                        ? modifier || key
+                          ? `<${modifier ? modifier + "+" : ""}${key || "???"}>`
+                          : "<Press Any Key>"
+                        : hotkey.hotkey}
+                    </b>
+                    {selectedHotKey === hotkey ? (
+                      <EditIcon style={hotkeyIconStyle} />
+                    ) : (
+                      <EditIcon style={hotkeyIconStyle} />
+                    )}
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </SimpleDialog>
   )
 }
