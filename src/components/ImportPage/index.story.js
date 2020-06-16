061bd91a53c606e7cd1ad60ae5bebfddce1ffe72
Mohammed Eldadah <mrdadah@gmail.com>
2020-06-16 15:18:37 +0300

    implement disable import text snippets

--- src/components/ImportPage/index.story.js
+++ src/components/ImportPage/index.story.js
@@ -89,3 +89,32 @@ storiesOf("ImportPage", module)
       onChangeDataset={action("onChangeDataset")}
     />
   ))
+  .add("Hide Import Text Snippets", () => (
+    <ImportPage
+      isDesktop={false}
+      dataset={{
+        interface: {
+          type: "image_segmentation",
+          labels: ["valid", "invalid"],
+          regionTypesAllowed: [
+            "bounding-box",
+            "polygon",
+            // "full-segmentation",
+            "point",
+            // "pixel-mask"
+          ],
+        },
+        samples: [
+          {
+            imageUrl:
+              "https://s3.amazonaws.com/asset.workaround.online/example-jobs/sticky-notes/image1.jpg",
+          },
+          {
+            imageUrl:
+              "https://s3.amazonaws.com/asset.workaround.online/example-jobs/sticky-notes/image2.jpg",
+          },
+        ],
+      }}
+      onChangeDataset={action("onChangeDataset")}
+    />
+  ))
