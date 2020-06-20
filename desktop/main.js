027ee139f915feffd20c8f5716ab6708168c39ce
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2020-06-20 21:57:29 +0300

    Add auto-update

--- desktop/main.js
+++ desktop/main.js
@@ -3,6 +3,7 @@ const { app, BrowserWindow, Menu, shell } = require("electron")
 const path = require("path")
 const menuTemplate = require("./menu-template")
 const { format: formatUrl } = require("url")
+const { autoUpdater } = require('electron-updater'); 
 
 function createWindow() {
   // Create the browser window.
@@ -63,7 +64,10 @@ app.on("window-all-closed", function () {
 app.on("activate", function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  if (BrowserWindow.getAllWindows().length === 0){
+    createWindow()
+    autoUpdater.checkForUpdatesAndNotify()
+  }
 })
 
 // In this file you can include the rest of your app's specific main process
