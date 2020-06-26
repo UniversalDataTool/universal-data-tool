dac0df73dbae2d5bcd5a861b04e2cf60c3076c31
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2020-06-26 19:24:22 +0300

    Made branch prettier

--- src/components/ImportFromGoogleDriveDialog/index.js
+++ src/components/ImportFromGoogleDriveDialog/index.js
@@ -20,7 +20,8 @@ const credentials = {
     client_id:
       process.env.REACT_APP_GOOGLE_DRIVE_CLIENT_ID ||
       "604137720705-vde9rqj1h6u35g9mr1pl7o4l594j6cl6.apps.googleusercontent.com",
-    app_id: process.env.REACT_APP_GOOGLE_DRIVE_APP_ID || "quickstart-1592363961559",
+    app_id:
+      process.env.REACT_APP_GOOGLE_DRIVE_APP_ID || "quickstart-1592363961559",
     developer_key:
       process.env.REACT_APP_GOOGLE_DRIVE_DEVELOPER_KEY ||
       "AIzaSyAemSdtGTv3wzRk8OnMuDruC53eLyFtXJQ",
@@ -151,7 +152,6 @@ export default ({ open, onClose, onAddSamples }) => {
   useEffect(() => {
     if (open) {
       onLoadPicker()
-     
     }
   }, [open, googleScriptLoaded, onLoadPicker])
 
