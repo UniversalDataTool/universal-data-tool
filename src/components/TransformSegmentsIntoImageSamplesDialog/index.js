2b49791c87aaee6ea29894be3a33d6ce1de3339d
seveibar <seveibar@gmail.com>
2020-06-26 10:23:25 -0400

    improvements for image segmentation splitting

--- src/components/TransformSegmentsIntoImageSamplesDialog/index.js
+++ src/components/TransformSegmentsIntoImageSamplesDialog/index.js
@@ -4,9 +4,6 @@ import React, { useState } from "react"
 import SimpleDialog from "../SimpleDialog"
 import { styled } from "@material-ui/core/styles"
 import { setIn } from "seamless-immutable"
-import Box from "@material-ui/core/Box"
-import Select from "react-select"
-import range from "lodash/range"
 
 const ErrorBox = styled("pre")({
   color: "red",
@@ -25,25 +22,27 @@ export default ({ open, onChangeDataset, onClose, dataset }) => {
         {
           text: "Convert Segments to Image Samples",
           onClick: async () => {
-            const imageUrls = Array.from(
-              new Set(dataset.samples.map((s) => s.imageUrl))
-            )
-            console.log({ imageUrls })
-            const newDataset = {
-              ...dataset,
-              samples: [],
-            }
-            for (const imageUrl of imageUrls) {
-              const pieces = dataset.samples.filter(
-                (s) => s.imageUrl === imageUrl
+            try {
+              const imageUrls = Array.from(
+                new Set(dataset.samples.map((s) => s.imageUrl))
               )
-              newDataset.samples.push({
-                imageUrl,
-                annotation: pieces.flatMap((p) => p.annotation).filter(Boolean),
-              })
+              const newSamples = []
+              for (const imageUrl of imageUrls) {
+                const pieces = dataset.samples.filter(
+                  (s) => s.imageUrl === imageUrl
+                )
+                newSamples.push({
+                  imageUrl,
+                  annotation: pieces
+                    .flatMap((p) => p.annotation)
+                    .filter(Boolean),
+                })
+              }
+              onChangeDataset(setIn(dataset, ["samples"], newSamples))
+              onClose()
+            } catch (e) {
+              setErrors(e.toString())
             }
-            onChangeDataset(newDataset)
-            onClose()
           },
         },
       ]}
