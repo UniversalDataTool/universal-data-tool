1be4a9463b78af174a18f7fb2f3223ee94245803
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2020-07-03 11:52:03 +0300

    Change fallback language to EN

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -18,7 +18,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: "cn",
+    fallbackLng: "en",
     debug: true,
 
     interpolation: {
