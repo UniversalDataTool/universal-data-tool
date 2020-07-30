da893303c187214f29b5111fd144c3bedd9e0441
seveibar <seveibar@gmail.com>
2020-07-30 14:54:32 -0400

    disable posthog when in Cypress test

--- src/utils/use-posthog.js
+++ src/utils/use-posthog.js
@@ -1,9 +1,39 @@
 import posthog from "posthog-js"
 import useElectron from "./use-electron"
 
+const posthogMock = {
+  opt_in_capturing: () => null,
+  opt_out_capturing: () => null,
+  init: () => null,
+  reset: () => null,
+  capture: () => null,
+  capture_forms: () => null,
+  capture_pageview: () => null,
+  register: () => null,
+  register_once: () => null,
+  unregister: () => null,
+  identify: () => "mock",
+  alias: () => null,
+  set_config: () => null,
+  get_config: () => null,
+  get_property: () => null,
+  get_distinct_id: () => null,
+  has_opted_out_capturing: () => true,
+  has_opted_in_capturing: () => false,
+  disable_all_events: () => null,
+  people: {
+    set: () => null,
+    set_once: () => null,
+  },
+}
+
 window.posthogInitialized = false
 export default () => {
   const isDesktop = Boolean(useElectron())
+  if (window.Cypress) {
+    window.posthogInitialized = true
+    window.posthog = posthogMock
+  }
   if (!window.posthogInitialized) {
     posthog.init("dSrx1PbFd02XsxlLd7qNbeTxKZwwao2sclVmapyYlT4", {
       api_host: "https://posthog.universaldatatool.com",
@@ -16,5 +46,5 @@ export default () => {
     posthog.people.set({ is_desktop: isDesktop, domain })
     window.posthogInitialized = true
   }
-  return posthog
+  return window.posthog
 }
