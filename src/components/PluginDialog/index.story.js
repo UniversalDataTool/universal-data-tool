27a970e03d2085e87894416d26a8ea367b70816e
seveibar <seveibar@gmail.com>
2020-09-27 16:40:53 -0400

    plugin dialog that can support a plugin renderDialog function

---
+++ src/components/PluginDialog/index.story.js
@@ -0,0 +1,41 @@
+// @flow
+
+import React from "react"
+
+import { storiesOf } from "@storybook/react"
+import { action } from "@storybook/addon-actions"
+
+import PluginDialog from "./"
+
+storiesOf("PluginDialog", module).add("Basic", () => {
+  const renderDialog = ({
+    elm,
+    dataset,
+    setInDataset,
+    addAction,
+    removeAction,
+    onSuccess,
+  }) => {
+    console.log({ elm })
+    elm.innerHTML = `
+
+      Click the "Delete All Samples" button to delete all of your samples!
+
+      `.trim()
+
+    addAction({
+      text: "Delete All Samples",
+      onClick: () => {
+        setInDataset(dataset, ["samples"], [])
+        onSuccess("All samples deleted!")
+      },
+    })
+  }
+  return (
+    <PluginDialog
+      open={true}
+      name="Delete All Samples"
+      renderDialog={renderDialog}
+    />
+  )
+})
