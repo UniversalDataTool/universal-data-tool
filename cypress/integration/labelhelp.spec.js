92be9b88064489f4c5cd4714f190fd8db45f61c9
seveibar <seveibar@gmail.com>
2020-10-16 07:23:18 -0400

    fix test to reflect Label Help -> Crowd Label

--- cypress/integration/labelhelp.spec.js
+++ cypress/integration/labelhelp.spec.js
@@ -9,7 +9,7 @@ describe("Create a Label Help ", () => {
     cy.contains("Import Toy Dataset").click()
     cy.get('[data-import-toy-dataset-name="Cats"]').click()
     cy.contains("Label").click()
-    cy.contains("Label Help").click()
+    cy.contains("Crowd Label").click()
     // This is a special api key that triggers mock functionality from the server
     // e.g. it always has 100 credits
     cy.get(".label-help-api-key-text-field").type(
