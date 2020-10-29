a35601971f0f8464b7a2e207001cedc4664fe393
seveibar <seveibar@gmail.com>
2020-10-29 16:55:39 -0400

    fixes #358 storybook recoil error

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { configure, addDecorator } from "@storybook/react"
 import Theme from "../src/components/Theme"
+import { RecoilRoot } from "recoil"
 import "../src/App.css"
 import "./storybook.css"
 import "../src/i18n"
@@ -21,8 +22,13 @@ export const themeDecorator = (storyFn) => {
   )
 }
 
+export const recoilDecorator = (storyFn) => {
+  return React.createElement(RecoilRoot, {}, storyFn())
+}
+
 function loadStories() {
   addDecorator(themeDecorator)
+  addDecorator(recoilDecorator)
   const importAll = (r) => r.keys().map(r)
   importAll(require.context("../src/components", true, /\.story\.js$/))
   importAll(require.context("../src/vanilla", true, /\.story\.js$/))
