271598dbffe8a046ee1e217325bd909184b042be
Cédric Jean <cedric.jean@momentum-tech.ca>
2020-12-02 16:01:55 -0500

    extract table-expanded-row from index.js

--- src/components/ImportFromCognitoS3Dialog/index.js
+++ src/components/ImportFromCognitoS3Dialog/index.js
@@ -10,6 +10,7 @@ import initConfigImport from "./init-config-import"
 import datasetHasChanged from "../../utils//dataset-helper/get-files-differences"
 import setUrl from "./set-url"
 import { setIn } from "seamless-immutable"
+import ExpandedRow from "./table-expanded-row"
 import {
   Settings as SettingsIcon,
   Storage as StorageIcon,
@@ -29,72 +30,8 @@ const tableStyle = {
   marginRight: "auto",
   width: "100%",
 }
-const expandedAssetsColumns = [
-  { name: "Assets", selector: "assets", sortable: true },
-  { name: "Last Modified", selector: "lastModified", sortable: true },
-]
-const expandedAnnotationsColumns = [
-  { name: "Annotations", selector: "annotation" },
-  { name: "Last Modified", selector: "lastModified", sortable: true },
-]
 
 const columns = [{ name: "Projects", selector: "folder", sortable: true }]
-
-const customStyles = {
-  headCells: {
-    style: {
-      paddingLeft: "10px",
-      "font-weight": "bold",
-    },
-  },
-  cells: {
-    style: {
-      paddingLeft: "25px",
-    },
-  },
-}
-
-const ExpandedRow = ({ data }) => {
-  const { rowAssets, rowAnnotations } = data
-  return (
-    <>
-      <DataTable
-        style={{
-          boxSizing: "border-box",
-          paddingLeft: "50px",
-          paddingRight: "10px",
-        }}
-        dense
-        striped
-        noHeader
-        columns={expandedAnnotationsColumns}
-        data={rowAnnotations}
-        noDataComponent='Make sure the project has "samples" folder'
-        pagination={rowAnnotations.length > 10}
-        paginationPerPage={10}
-        paginationRowsPerPageOptions={[10, 20, 25, 50, 100, 200]}
-        customStyles={customStyles}
-      />
-      <DataTable
-        style={{
-          boxSizing: "border-box",
-          paddingLeft: "50px",
-          paddingRight: "10px",
-        }}
-        dense
-        striped
-        noHeader
-        columns={expandedAssetsColumns}
-        data={rowAssets}
-        noDataComponent={'Make sure the project has "data" folder'}
-        pagination={rowAssets.length > 10}
-        paginationPerPage={10}
-        paginationRowsPerPageOptions={[10, 20, 25, 50, 100, 200]}
-        customStyles={customStyles}
-      />
-    </>
-  )
-}
 
 export default ({ open, onClose, onAddSamples }) => {
   const [dm, setDm] = useState(null)
@@ -102,9 +39,7 @@ export default ({ open, onClose, onAddSamples }) => {
   const { authConfig } = useAuth()
   const [projects, setProjects] = useState(null)
   const [projectToFetch, setProjectToFetch] = useState("")
-  const [configImport, setConfigImport] = useState(
-    initConfigImport(oldData)
-  )
+  const [configImport, setConfigImport] = useState(initConfigImport(oldData))
   const lastObjectRef = useRef({})
 
   const hasProjectStarted = useCallback(() => {
@@ -122,7 +57,7 @@ export default ({ open, onClose, onAddSamples }) => {
     var configToSet = configImport
     const changes = datasetHasChanged(lastObjectRef.current, oldData)
     if (changes.interface.type || changes.samples) {
-        configToSet = setTypeOfFileToLoadAndDisable(configToSet, oldData)
+      configToSet = setTypeOfFileToLoadAndDisable(configToSet, oldData)
     }
     setConfigImport({
       ...configToSet,
@@ -256,7 +191,8 @@ export default ({ open, onClose, onAddSamples }) => {
           disabled: configImport.contentDialogBoxIsSetting,
         },
       ]}
-    >{console.log("render")}
+    >
+      {console.log("render")}
       <table style={tableStyle}>
         <tbody>
           <tr>
