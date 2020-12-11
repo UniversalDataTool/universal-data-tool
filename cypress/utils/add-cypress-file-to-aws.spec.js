58ae00456dab18f44c22f44d2e76b77594dd9065
Cédric Jean <cedric.jean@momentum-tech.ca>
2020-12-11 16:59:41 -0500

    Test fonctionnel

---
+++ cypress/utils/add-cypress-file-to-aws.spec.js
@@ -0,0 +1,28 @@
+import datasetManagerCognito from "udt-dataset-managers/dist/CognitoDatasetManager"
+
+const removeAWSFile = (name) => {
+  cy.fixture("credentials.json").then((credentials) => {
+    cy.fixture("samples-dummies.json").then(async (dummies) => {
+      const authConfig = {
+        Auth: {
+          identityPoolId: credentials.CYPRESS_AWS_IDENTITY_POOL_ID,
+          region: credentials.CYPRESS_AWS_AUTH_REGION,
+          userPoolId: credentials.CYPRESS_AWS_USER_POOL_ID,
+          userPoolWebClientId: credentials.CYPRESS_AWS_USER_POOL_WEB_CLIENT_ID,
+          mandatorySignIn: credentials.CYPRESS_AWS_MANDATORY_SIGN_IN,
+          authenticationFlowType:
+            credentials.CYPRESS_AWS_AUTHENTICATION_FLOW_TYPE,
+        },
+        Storage: {
+          AWSS3: {
+            bucket: credentials.CYPRESS_AWS_STORAGE_BUCKET,
+            region: credentials.CYPRESS_AWS_STORAGE_REGION,
+          },
+        },
+      }
+      const ds = await new datasetManagerCognito({ authConfig })
+      await ds.setDataset(dummies)
+    })
+  })
+}
+export default removeAWSFile
