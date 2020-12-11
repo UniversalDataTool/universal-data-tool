49eafa831d1d40b1fd91a87ab200485d14f96a54
Cédric Jean <cedric.jean@momentum-tech.ca>
2020-12-11 14:44:29 -0500

    change file tree

--- cypress/integration/aws-test/export-aws.spec.js
+++ cypress/integration/aws-test/export-aws.spec.js
@@ -1,20 +1,22 @@
-import enterCredentialsCognitoS3 from "../utils/credentials-test/enter-credentials-cognito-s3.spec"
-import enterCredentialsUser from "../utils/credentials-test/enter-credentials-user.spec"
-import setLanguage from "../utils/set-language.spec"
-import goToImportPage from "../utils/go-to-import-page.spec"
-import removeAWSFile from "../utils/remove-cypress-file-in-aws.spec"
+import enterCredentialsCognitoS3 from "../../utils/credentials-test/enter-credentials-cognito-s3.spec"
+import enterCredentialsUser from "../../utils/credentials-test/enter-credentials-user.spec"
+import setLanguage from "../../utils/set-language.spec"
+import goToImportPage from "../../utils/go-to-import-page.spec"
+import removeAWSFile from "../../utils/remove-cypress-file-in-aws.spec"
 import "regenerator-runtime/runtime"
 describe("aws test", () => {
-  it("Try to export a natif project to aws", () => {
+  before("Prepare tests", () => {
     cy.log("should be able to join the web site")
     cy.visit(`http://localhost:6001`)
-
     cy.wait(400)
+
     setLanguage()
     enterCredentialsCognitoS3()
     enterCredentialsUser()
     goToImportPage()
+  })
 
+  it("Try to export a natif project to aws", () => {
     cy.log("should be able to use export project")
     cy.wait(200)
     cy.get(
@@ -38,11 +40,13 @@ describe("aws test", () => {
     cy.wait(200)
     cy.contains("CypressTest1")
     cy.contains("Close").click()
+  })
 
+  afterEach("Clean AWS", () => {
     removeAWSFile("CypressTest1")
 
     cy.log("should not be able to see the new project")
-    cy.wait(2000)
+    cy.wait(200)
     cy.get(
       "button[class='MuiButtonBase-root MuiButton-root MuiButton-outlined WithStyles(ForwardRef(Button))-root-110']"
     )
@@ -50,5 +54,6 @@ describe("aws test", () => {
       .click()
     cy.wait(200)
     cy.contains("CypressTest1").should("not.exist")
+    cy.contains("Close").click()
   })
 })
