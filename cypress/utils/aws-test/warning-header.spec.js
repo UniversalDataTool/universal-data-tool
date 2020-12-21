569464fea4bc933c1e5abbafcdffdfeb69831a53
Cédric Jean <cedric.jean@momentum-tech.ca>
2020-12-21 14:22:44 -0500

    add test going with

--- cypress/utils/aws-test/warning-header.spec.js
+++ cypress/utils/aws-test/warning-header.spec.js
@@ -1,14 +1,23 @@
 const warningHeader = () => {
-  it("Check Header Warning", () => {
+  it("Warning project name not set", () =>{
+    cy.contains("Export to S3 (Cognito)").click()
+    cy.contains(
+      "Warning : Please enter a project name."
+    )
+    cy.contains("Close").click()
+  })
+  it("Warning project already exist", () => {
     cy.contains("Export to S3 (Cognito)").click()
     cy.get("input[id='ProjectName']")
       .focus()
       .clear()
       .type("Image Classification")
     cy.contains(
-      "Warning : This project name already exist. If you continue the existing project with the same name will be replaced"
+      "Warning : This project name already exist. If you continue the existing project with the same name will be replaced."
     )
     cy.contains("Close").click()
+  })
+  it("Warning select a project", () => {
     cy.contains("Import from S3 (Cognito)").click()
     cy.contains("Warning : You need to select a project.")
     cy.contains("Close").click()
