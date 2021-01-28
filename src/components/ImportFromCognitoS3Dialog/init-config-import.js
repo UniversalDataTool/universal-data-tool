c8f8a420e9e7bc768e133721d4e99b6cdb9e88e8
Cédric Jean <cedric.jean@momentum-tech.ca>
2021-01-28 11:49:45 -0500

    Texte to Text

--- src/components/ImportFromCognitoS3Dialog/init-config-import.js
+++ src/components/ImportFromCognitoS3Dialog/init-config-import.js
@@ -14,11 +14,25 @@ export default (dataset) => {
       ? "Text"
       : "None",
     typeOfFileToDisable: {
+<<<<<<< HEAD
       Image: !checkInterfaceAndsamples(["Image", "Empty"], dataset),
       Video: !checkInterfaceAndsamples(["Video", "Empty"], dataset),
       Audio: !checkInterfaceAndsamples(["Audio", "Empty"], dataset),
       PDF: !checkInterfaceAndsamples(["PDF", "Empty"], dataset),
       Text: !checkInterfaceAndsamples(["Text", "Empty"], dataset),
+=======
+      Image: checkInterfaceAndsamples(["Image", "Empty"], dataset)
+        ? false
+        : true,
+      Video: checkInterfaceAndsamples(["Video", "Empty"], dataset)
+        ? false
+        : true,
+      Audio: checkInterfaceAndsamples(["Audio", "Empty"], dataset)
+        ? false
+        : true,
+      PDF: checkInterfaceAndsamples(["PDF", "Empty"], dataset) ? false : true,
+      Text: checkInterfaceAndsamples(["Text", "Empty"], dataset) ? false : true,
+>>>>>>> 0130358 (Texte to Text)
     },
     loadAssetsIsSelected: true,
     contentDialogBoxIsSetting: false,
