a5e9b137bd0d0f4130fc200902cd8ac4e89906e9
Cédric Jean <cedric.jean@momentum-tech.ca>
2021-01-29 14:52:06 -0500

    Refactor cognito test

--- cypress/integration/utils/go-to-import-page.js
+++ cypress/integration/utils/go-to-import-page.js
@@ -1,6 +1,6 @@
 const test = (typeProject) => {
   cy.log("should be able to go to the import page")
-  cy.contains("New File").click()
+  cy.contains("New File", { timeout: 50000 }).click()
   if (typeProject) cy.contains(typeProject).click()
   cy.get('button[id="tab-samples"]').click()
   cy.contains("Samples").click()
