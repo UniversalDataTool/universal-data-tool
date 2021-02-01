62f4a0faf105f77f5b25f4f273104abc0cd68968
Cédric Jean <cedric.jean@momentum-tech.ca>
2021-02-01 10:38:24 -0500

    Silent i18n

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -21,7 +21,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: "en",
-    debug: true,
+    debug: false,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
