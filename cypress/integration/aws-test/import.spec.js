fb3573b744c97838a711dc0c7e172a0aef1722fa
Cédric Jean <cedric.jean@momentum-tech.ca>
2021-03-19 11:29:08 -0400

    Common part for import test

---
+++ cypress/integration/aws-test/import.spec.js
@@ -0,0 +1,40 @@
+import commandLocalStorage from "../utils/cypress-command/local-storage"
+import commandCredentialsAws from "../utils/cypress-command/credentials-aws"
+import commandCleanAws from "../utils/cypress-command/clean-aws"
+import commandSetUpAws from "../utils/cypress-command/set-up-aws"
+import commandSetLanguage from "../utils/cypress-command/set-language"
+
+commandLocalStorage()
+commandCredentialsAws()
+commandSetUpAws()
+commandCleanAws()
+commandSetLanguage()
+
+Cypress.config("defaultCommandTimeout", 3000)
+if (Cypress.env().AWS_IDENTITY_POOL_ID)
+  describe("Import aws test", () => {
+    before("Prepare tests", () => {
+      cy.log("should be able to join the web site")
+      cy.visit(`http://localhost:6001`)
+      cy.setLanguage("en")
+      //the following are very long processus try to keep them here so they execute only once
+      cy.createCredentialsAws()
+      cy.saveLocalStorage()
+      cy.cleanAws()
+      cy.setUpAws()
+    })
+
+    beforeEach("Go to import page", () => {
+      cy.restoreLocalStorage()
+    })
+
+    //Comment below when debugging 1 test
+    afterEach("Return to home page", () => {
+      cy.get("button[title='Exit to Welcome Page']").click({ force: true })
+    })
+
+    //Comment below when debugging aws
+    after("Clean AWS", () => {
+      cy.cleanAws()
+    })
+  })
